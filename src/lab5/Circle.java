7ad5a8b819138dc0a64b4e54f7b787a633c8a64f
Nabil Qureshi <nabil.the.great.2000@gmail.com>
2025-10-23 16:36:30 -0400

    feat: add Circle class with constructors, area and perimeter calculation methods

---
+++ src/lab5/Circle.java
@@ -0,0 +1,33 @@
+package lab5;
+
+import java.lang.Math;
+
+public class Circle {
+	
+	Point center;
+	double radius;
+
+	public Cicle() {
+		center = new Point(0, 0);
+		radius = 1;
+	}
+
+	public Circle(Point p, double r) {
+		center = p;
+		radius = r;
+	}
+
+	public Circle(double x, double y, double r) {
+		center = new Point(x, y);
+		radius = r;
+	}
+
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+
+	public double perimeter() {
+		return 2 * Math.PI * radius;
+	}
+
+}
