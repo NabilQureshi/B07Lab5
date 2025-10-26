c428a0ddf549f2ee16981d099a5f150c7c766ab3
omaretskyeh <omaretskyeh@gmail.com>
2025-10-26 18:47:11 -0400

    Add additional Rectangle test cases

--- src/lab5/RectangleTest.java
+++ src/lab5/RectangleTest.java
@@ -3,23 +3,40 @@ package lab5;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-public class RectangleTest{
-
-    @Test
-    void testArea() {
-        Rectangle r = new Rectangle(new Point(0, 0), 4, 5);
-        assertEquals(20, r.area());
-    }
-
-    @Test
-    void testPerimeter() {
-        Rectangle r = new Rectangle(new Point(1, 1), 3, 6);
-        assertEquals(18, r.perimeter());
-    }
+public class RectangleTest {
 
     @Test
     void testDefaultConstructor() {
         Rectangle r = new Rectangle();
-        assertEquals(1, r.area());
+        assertEquals(1.0, r.area(), 0.001);
+        assertEquals(4.0, r.perimeter(), 0.001);
+    }
+
+    @Test
+    void testConstructorWithPoint() {
+        Point p = new Point(2, 3);
+        Rectangle r = new Rectangle(p, 4, 5);
+        assertEquals(20.0, r.area(), 0.001);
+        assertEquals(18.0, r.perimeter(), 0.001);
+    }
+
+    @Test
+    void testConstructorWithCoordinates() {
+        Rectangle r = new Rectangle(1, 1, 2, 3);
+        assertEquals(6.0, r.area(), 0.001);
+        assertEquals(10.0, r.perimeter(), 0.001);
+    }
+
+    @Test
+    void testAreaCalculation() {
+        Rectangle r = new Rectangle(new Point(0, 0), 3, 7);
+        assertEquals(21.0, r.area(), 0.001);
+    }
+
+    @Test
+    void testPerimeterCalculation() {
+        Rectangle r = new Rectangle(new Point(0, 0), 3, 7);
+        assertEquals(20.0, r.perimeter(), 0.001);
     }
 }
+
